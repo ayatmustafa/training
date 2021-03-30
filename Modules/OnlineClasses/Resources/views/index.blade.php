fedf3754a20159a580e05743046d6c54e401cec6
m.ahmedmedo <m.ahmedmedo457@gmail.com>
2021-03-30 17:31:43 +0200

    Configration

--- Modules/OnlineClasses/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('onlineclasses::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('onlineclasses.name') !!}
-    </p>
-@endsection
