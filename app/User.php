839b6bd76d102a3d9ce86751f1147aa09f22f07f
m.ahmedmedo <m.ahmedmedo457@gmail.com>
2021-03-30 13:53:03 +0200

    User Authentication with laravel passport

--- app/User.php
+++ app/User.php
@@ -3,12 +3,13 @@
 namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -25,7 +26,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token','created_at', 'updated_at', 'email_verified_at',
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
 });
-Route::get('/index', function() {
-    return "d7ka";
+
+Route::group(['middleware' => ['cors', 'json.response']], function () {
+    Route::post('/login', 'Auth\ApiAuthController@login')->name('login.api');
+    Route::post('/register', 'Auth\ApiAuthController@register')->name('register.api');
+    Route::post('/logout', 'Auth\ApiAuthController@logout')->name('logout.api');
 });
