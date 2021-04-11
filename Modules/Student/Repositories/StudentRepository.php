0e183e2988f0af3726c1165352017d84c144b860
unknown <hebamohey17>
2021-04-11 12:44:05 +0200

    student for

--- Modules/Student/Repositories/StudentRepository.php
+++ Modules/Student/Repositories/StudentRepository.php
@@ -11,41 +11,45 @@ use Modules\Student\Entities\Student;
 
 class StudentRepository implements StudentRepositoryInterface
 {
-    public function create($studentData)
-    {
-        $user = Auth::user()->id;
-      return Student::create(array_merge($studentData->all(),['user_id'=> $user]));
+  public function create($studentData)
+  {
+    $user = Auth::user()->id;
+    $student=Student::create(array_merge($studentData->all(), ['user_id' => $user]));
+    return  $student;
+  }
+  public function index()
+  {
+    return Student::all();
+  }
+  public function edit($student_id)
+  {
 
-    }
-    public function index()
-    {
-      return Student::all();
-    }
-    public function edit($student_id)
-    {
-       
-        return Student::where('id', $student_id)->first();
-    }
-    public function Update($student_id, $studentdata)
-    {
-        $student = Student::where('id', $student_id)->first();
-      $student->update($studentdata->all());
-      return    $student; 
-    }
-    public function StudentsInDivision($division_id)
-    {
-      return Student::where('division_id', $division_id)->get();
-    }
-    public function StudentsInSection($section_id)
-    { 
-        return Student::where('section_id',$section_id)->get();
-    }
-    public function StudentsInGrade($grade_id)
-    {
-       return Student::where('grade_id',$grade_id);
-    }
-    public function AddStudentToGrade($id,$req)
-    {
-     return Student::where('id',$id)->update(['grade_id'=>$req->grade_id]);
-    }
+    return Student::where('id', $student_id)->first();
+  }
+  public function Update($student_id, $studentdata)
+  {
+    $student = Student::where('id', $student_id)->first();
+    $student->update($studentdata->all());
+     return $student;
+  }
+  public function StudentsInDivision($division_id)
+  {
+    $student = Student::where('division_id', $division_id)->get();
+    return    $student;
+  }
+  public function StudentsInSection($section_id)
+  {
+    $student = Student::where('section_id', $section_id)->get();
+    return  $student;
+  }
+  public function StudentsInGrade($grade_id)
+  {
+    $student = Student::where('grade_id', $grade_id);
+    return  $student;
+  }
+  public function AddStudentToGrade($id, $req)
+  {
+    $student = Student::where('id', $id)->update(['grade_id' => $req->grade_id]);
+    return $student;
+  }
 }
